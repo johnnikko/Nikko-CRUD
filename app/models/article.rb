befe2500eff007441f3ce4210330406956cd402f
johnnikko <nikko06albarado@gmail.com>
2019-08-10 17:33:50 +0800

    Add association between articles and category

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ActiveRecord::Base
   belongs_to :user
-  validates :title, presence: true
-  validates :description, presence: true
+  has_many :article_categories
+  has_many :categories, through: :article_categories
+  validates :title, presence: true, length: {minimun:3,maximum:50}
+  validates :description, presence: true, length: {minimun:10,maximum:300}
   validates :user_id, presence: true
 end
