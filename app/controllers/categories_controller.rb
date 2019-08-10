df58bd5aa153385501afb2badedcf80fa8487f55
johnnikko <nikko06albarado@gmail.com>
2019-08-10 18:34:48 +0800

    add update in categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,7 +12,7 @@ class CategoriesController < ApplicationController
   def create
     @category = Category.new(category_params)
     if @category.save
-      flash[:success] = "Category was created succesfull"
+      flash[:success] = "Category name was created succesful."
       redirect_to categories_path
     else
       render 'new'
@@ -22,6 +22,20 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Category name was created succesfully updated."
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
   end
 
   private
